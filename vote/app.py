4de39389ea666dbd59b36275c8beb6f250e853d7
Dinesh8352 <166284817+Dinesh8352@users.noreply.github.com>
2025-08-29 18:09:36 +0530

    Tiger added.py

--- vote/app.py
+++ vote/app.py
@@ -7,7 +7,7 @@ import json
 import logging
 
 option_a = os.getenv('OPTION_A', "Cats or dogs")
-option_b = os.getenv('OPTION_B', "Tigers")
+option_b = os.getenv('OPTION_B', "BengalTigers")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
