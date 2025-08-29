01092e8394e5bf6cbf077157b5a8e3bdb5b9e1a8
Dinesh8352 <kdineshteja55@gmail.com>
2025-08-29 18:04:25 +0530

    animals added

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
+option_a = os.getenv('OPTION_A', "Cats or dogs")
 option_b = os.getenv('OPTION_B', "Tigers")
 hostname = socket.gethostname()
 
