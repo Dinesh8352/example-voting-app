0a483c2274528699e8c32153978f4305118d2de8
Dinesh8352 <kdineshteja55@gmail.com>
2025-08-29 17:34:14 +0530

    app names changd

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "teja")
-option_b = os.getenv('OPTION_B', "Dinesh")
+option_a = os.getenv('OPTION_A', "tejaboss")
+option_b = os.getenv('OPTION_B', "Dineshsemiboss")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
